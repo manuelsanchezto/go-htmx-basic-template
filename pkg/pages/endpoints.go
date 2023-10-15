8f63114e2af7affee169ead1f323f45541892d11
manuel sanchez torres <manuel.san.torr@gmail.com>
2023-10-15 16:45:55 +0200

    Propuesta prueba chatMsg

--- pkg/pages/endpoints.go
+++ pkg/pages/endpoints.go
@@ -5,8 +5,23 @@ import (
 )
 
 type IndexPage struct {
+	Messages []Message
+}
+
+type Message struct {
+	Text       string
+	IsFromUser bool
+	Time       string
 }
 
 func Index(c echo.Context) error {
-	return c.Render(200, "index.html", IndexPage{})
+	Messages := getMessages()
+	return c.Render(200, "index.html", IndexPage{
+		Messages: Messages,
+	})
+}
+
+func getMessages() []Message {
+	var messages []Message
+	return messages
 }
